66d0fe84fcc3f9e58ed38f6b977bca3afba98bf2
Tracer16 <tracermagee@gmail.com>
2018-06-14 20:09:53 -0500

    updated routes

---
+++ routes/html-routes.js
@@ -0,0 +1,29 @@
+// routes to send users to html pages
+
+//dependencies
+var path = require("path");
+
+//Routes
+module.exports = function(app) {
+
+    //index route loads view.html
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/sessions.html"));
+    });
+
+    //cms route loads cms.html
+    app.get("/cms", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/cms.html"));
+    });
+
+    //sessions route loads sessions.html
+    app.get("/sessions", function(req, res) {
+        res.sendFile(path.join(__dirname, "..public/sessions.html"));
+    });
+
+    //player route loads player-manager.html
+    app.get("/players", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/player-manager.html"));
+    });
+
+};
